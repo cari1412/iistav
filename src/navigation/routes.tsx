cf5c43aac653afd68a89b2d9aec224e80ed8025a
cari1412 <vadis1766@gmail.com>
2025-02-07 13:39:11 +0300

    2

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,8 +1,6 @@
 import type { ComponentType, JSX } from 'react';
 
 import { IndexPage } from '@/pages/IndexPage/IndexPage';
-import { InitDataPage } from '@/pages/InitDataPage.tsx';
-import { LaunchParamsPage } from '@/pages/LaunchParamsPage.tsx';
 import { ThemeParamsPage } from '@/pages/ThemeParamsPage.tsx';
 import { TONConnectPage } from '@/pages/TONConnectPage/TONConnectPage';
 
@@ -15,9 +13,7 @@ interface Route {
 
 export const routes: Route[] = [
   { path: '/', Component: IndexPage },
-  { path: '/init-data', Component: InitDataPage, title: 'Init Data' },
   { path: '/theme-params', Component: ThemeParamsPage, title: 'Theme Params' },
-  { path: '/launch-params', Component: LaunchParamsPage, title: 'Launch Params' },
   {
     path: '/ton-connect',
     Component: TONConnectPage,
